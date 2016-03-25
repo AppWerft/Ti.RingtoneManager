d8edd2869d13477a68ca5dd878cb4f972d480553
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-03-25 16:08:37 +0100

         

--- android/example/app.js
+++ android/example/app.js
@@ -3,27 +3,28 @@
 // to test out the module and to provide instructions
 // to users on how to use it by example.
 
-
 // open a single window
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+	backgroundColor : 'white'
 });
-var label = Ti.UI.createLabel();
-win.add(label);
-win.open();
 
-var RingTone = require('de.appwerft.ringtonmanager');
-var xhr = Ti.Network.createHTTPClient({
+win.open();
+win.addEventListener('click', function() {
+	var RingTone = require('de.appwerft.ringtonmanager');
+	var xhr = Ti.Network.createHTTPClient({
 		onload : function() {
-			var soundfile = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'ringtones', 'sound.mp3');
+			var soundfile = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'sound.mp3');
 			soundfile.write(this.responseData);
 			RingTone.setActualDefaultRingtone({
 				url : soundfile.nativePath,
 				title : 'Aegyptisches_Dendrawahuhn'
 			});
-
+			RingTone.addEventListener('success',function(){
+				
+			});
 		}
 	});
-	xhr.open('GET', 'http://www.tierstimmenarchiv.de/recordings/0008_Aegyptisches_Dendrawahuhn_short.mp3');  // from http://www.tierstimmenarchiv.de/recordings/
+	xhr.open('GET', 'http://www.tierstimmenarchiv.de/recordings/0008_Aegyptisches_Dendrawahuhn_short.mp3');
 	xhr.send();
+});
 
